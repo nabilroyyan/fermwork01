b7925c1934ecbe59be727fefd83ff318efc1fc84
nabil royyan <nabil.royyan1203@gmail.com>
2024-06-13 13:23:32 +0700

    struk2

--- routes/perizinan.js
+++ routes/perizinan.js
@@ -78,4 +78,27 @@ router.get("/", async function (req, res, next) {
     }
 });
 
+router.get('/invois/:id', async function(req, res, next) {
+    try {
+        let id = req.params.id;
+        let menu = await model_boking.getById(id);
+        function formatDate(dateString) {
+            const date = new Date(dateString);
+            date.setDate(date.getDate() + 1); // Adjust for timezone offset
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        }
+        res.render('nota/invois', {
+            menu: menu,
+            formatDate
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        req.flash('error', 'Gagal memuat halaman edit menu');
+        res.redirect('nota/');
+    }
+  });
+
 module.exports = router
